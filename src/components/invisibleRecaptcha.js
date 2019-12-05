e930ac2302181841ab9c8a34c3ae31754a2a17b6
Marcell Spies <marcells@gmx.de>
2019-12-05 23:23:11 +0100

    Added recaptcha check, cause of page reload issue (https://github.com/appleboy/react-recaptcha/issues/262).

--- src/components/invisibleRecaptcha.js
+++ src/components/invisibleRecaptcha.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 import Recaptcha from 'react-recaptcha'
+import useSSR from 'use-ssr'
 
 const InvisibleRecaptcha = (props) => {
   let recaptchaInstance
 
+  const { isBrowser } = useSSR();
   const verifyCallback = token => { props.onVerified(token) }
 
   useEffect(() => {
@@ -11,9 +13,11 @@ const InvisibleRecaptcha = (props) => {
     recaptchaInstance.execute()
   }, [])
 
+  if (!isBrowser)
+    return <React.Fragment />
+
   return (
     <React.Fragment>
-
       <small>
         This site is protected by reCAPTCHA and the Google <a href="https://policies.google.com/privacy">Privacy Policy</a> and <a href="https://policies.google.com/terms">Terms of Service</a> apply.
       </small>
