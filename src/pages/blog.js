b971c4dd57cd1015355d339b282544814eb750b9
Marcell Spies <marcells@gmx.de>
2019-12-23 00:58:05 +0100

    Display posts in a better way.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import { graphql, Link } from 'gatsby'
+import { Container, CardDeck, Card } from 'react-bootstrap'
 import Layout from '../components/layout'
 
 export const query = graphql`
     query BlogQuery {
-      allMarkdownRemark {
+      allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
         edges {
           node {
             frontmatter {
               path
               title
+              date(formatString: "MMMM DD, YYYY")
             }
             id
             excerpt
@@ -19,24 +21,24 @@ export const query = graphql`
     }
 `
 
+const BlogEntry = ({ node }) => 
+  <Card>
+    <Card.Body>
+      <Card.Title>
+        <Link to={`/blog/${node.frontmatter.path}/`}>
+          {node.frontmatter.title}
+        </Link>
+      </Card.Title>
+      <Card.Subtitle className="mb-2 text-muted">{node.frontmatter.date}</Card.Subtitle>
+      <Card.Text>{node.excerpt}</Card.Text>
+    </Card.Body>
+  </Card>
+
 const BlogPage = ({ data }) => (
   <Layout>
-    <main>
-      <h1>Blogartikel</h1>
+    <h1>Blogartikel</h1>
 
-      <ul>
-        {
-          data.allMarkdownRemark.edges.map(({ node }) =>
-            <li key={node.id}>
-              <Link to={`/blog/${node.frontmatter.path}/`}>
-                {node.frontmatter.title}
-              </Link>
-              <br />
-              <span>{node.excerpt}</span>
-            </li>)
-        }
-      </ul>
-    </main>
+    { data.allMarkdownRemark.edges.map(({ node }) => <BlogEntry key={node.id} node={node} />) }
   </Layout>
 )
 
